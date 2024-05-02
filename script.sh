8c0c4f9d5b275fc6b163c79336f364292af44de4
rkmchittu-at-851725573285 <ubuntu@ip-172-31-31-247.us-west-2.compute.internal>
2024-05-02 15:07:56 +0000

    Adding script file

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Your script content here
+echo "Hello, world! This script was executed from Jenkins."
+
+# Example data storage (replace with your data handling)
+data="This is some sample data"
+echo "Data: $data"
