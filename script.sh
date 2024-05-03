6ba27b843088a5e6ba605136e5556b42e9f57f28
rkmchittu-at-851725573285 <ubuntu@ip-172-31-31-247.us-west-2.compute.internal>
2024-05-03 09:58:05 +0000

    modified script file

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ echo "Hello, world! This script was executed from Jenkins."
 
 # Example data storage (replace with your data handling)
 data="This is some sample data"
-echo "Data: $data"
+echo "Data output: $data"
